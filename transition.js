033ffe3315eae05699dd0b596d32682a704a58b0
ninj0x <ninj0x@mininin.(none)>
2012-08-26 23:52:36 +0100

    initial

---
+++ transition.js
@@ -0,0 +1,93 @@
+window.onload = function() {
+	trans = new Transition();
+};
+
+function Transition(options){
+	this._init(options);
+	return this;
+}
+
+
+Transition.prototype = {
+	defaults: function() {
+		var self = this;
+		return {
+		name: 'Transition.js',
+		contentClass: 'content',
+		linkSelector: '.transition',
+		oldClass: 'transition-old',
+		newClass: 'transition-new',
+		transistionFunc: self.fade
+		};
+	},
+
+	go: function(href) {
+		this._swapContent(href);
+		history.pushState(null, null, href);
+	},
+
+	_init: function(options) {
+		this.firstRun = true; // Prevent popstate from reloading on first run.
+		this._createSettings(options);
+		this.go = this.go.bind(this);
+		this._swapContent = this._swapContent.bind(this);
+
+		$(this.settings.linkSelector).click(this._clicked());
+
+		var self = this;
+		window.addEventListener("popstate", function(e) {
+			self._swapContent(location.pathname);
+		});
+	},
+
+	_createSettings: function(options) {
+		options = options || {};
+		this.settings = $.extend({}, this.defaults(), options);
+	},
+
+	_clicked: function() {
+		var self = this;
+		return function(e) {
+			e.preventDefault();
+			self.go(this.href);
+		};
+	},
+
+	_swapContent: function(href) {
+		if (this.firstRun){
+			this.firstRun = false;
+			return;
+		}
+		var self = this;
+		var set = self.settings;
+		$.get(href, function(data) {
+			content = self._getContent(data, set.contentClass);
+
+			$("." + set.contentClass).children().addClass(set.oldClass);
+
+			$(content.innerHTML).addClass(set.newClass)
+			.hide()
+			.appendTo($("." + set.contentClass));
+
+			set.transistionFunc(set.oldClass, set.newClass, function() {
+				$("." + set.newClass).removeClass(set.newClass);
+				$("." + set.oldClass).remove();
+			});
+		});
+	},
+
+	_getContent: function(html, selector) {
+		var content = document.createElement('div');
+		content.innerHTML = html;
+		content = content.getElementsByClassName(selector)[0];
+		return content;
+	},
+
+	fade: function(oldClass, newClass, callback) {
+		$("." + oldClass).fadeOut('', function() {
+			$("." + newClass).fadeIn('', callback);
+		});
+
+	}
+};
+
